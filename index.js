d7619923c2f3d44a8ca005455092210cc1605187
Hunter <farmerhunter@proton.me>
2023-02-08 17:55:38 -0500

    added intents

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 require("dotenv").config(); 
 const { REST } = require('@discordjs/rest');
 const { Routes } = require("discord-api-types");
+const { Client, Intents, Collection } = require('discord.js');
+
+const fs = require('fs');
+const path = require('path');
+
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+	intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.MessageContent,
+		GatewayIntentBits.GuildMembers,
+	],
+});
